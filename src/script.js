8af8d5c6be1d541d8de34901e382232e76625b8f
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2024-02-12 23:47:15 +0530

    added : ghosts to the scene

--- src/script.js
+++ src/script.js
@@ -182,6 +182,9 @@ for (let i = 0; i < 50; i++) {
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
 
+  // cast grave shadow
+  grave.castShadow = true;
+
   graves.add(grave);
 }
 
@@ -218,6 +221,7 @@ gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
 gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
+
 scene.add(moonLight);
 
 // ############# Door Light ##############
@@ -230,6 +234,19 @@ house.add(doorLight);
 // ####### Fog #####
 const fog = new THREE.Fog("#262837", 1, 15);
 scene.fog = fog;
+
+/**
+ * Ghosts
+ */
+
+const ghost1 = new THREE.PointLight("#ff00ff", 2, 3);
+scene.add(ghost1);
+
+const ghost2 = new THREE.PointLight("#00ffff", 2, 3);
+scene.add(ghost2);
+
+const ghost3 = new THREE.PointLight("#ffff00", 2, 3);
+scene.add(ghost3);
 
 /**
  * Sizes
@@ -290,6 +307,44 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Sets the color of the background of the scene
 renderer.setClearColor("#262837");
 
+// Activate the shadows
+renderer.shadowMap.enabled = true;
+// Add shadow algorithm
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// Activate the shadows on the lights that cast shadows
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+
+// Walls shadows
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+// floor to recieve shadow
+floor.receiveShadow = true;
+
+// ############### Optimisation of shadows ###############
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
+
 /**
  * Animate
  */
@@ -297,6 +352,23 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animating ghosts
+  const ghost1Angle = elapsedTime * 0.5;
+  ghost1.position.x = Math.cos(ghost1Angle) * 4;
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y = Math.sin(elapsedTime * 3);
+
+  const ghost2Angle = -elapsedTime * 0.32;
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+
+  const ghost3Angle = -elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
+  ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
 
   // Update controls
   controls.update();
